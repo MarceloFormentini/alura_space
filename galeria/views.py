8e1d39c3885e9e6ae0b51fe6332d4d0ea71a6f70
MarceloFormentini <marceloformentini74@gmail.com>
2025-12-15 22:14:18 -0300

    Adicionado pesquisa de imagem

--- galeria/views.py
+++ galeria/views.py
@@ -9,3 +9,16 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        termo_busca = request.GET['buscar']
+        if termo_busca:
+            fotografias = fotografias.filter(
+                nome__icontains=termo_busca
+            )
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
